28f9560586d24758d860870c41aec79724726fe3
t0pgunn <gzemerin@u.rochester.edu>
2016-10-02 20:03:53 -0400

    navbar is now a php inc

--- genre2.php
+++ genre2.php
@@ -28,39 +28,13 @@
 
 <body>
 
-    <!-- Navbar -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- toggle nav bar -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">
-                    <img src="http://placehold.it/150x50&text=Logo" alt="">
-                </a>
-            </div>
-            <!-- navbar content -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">Genre 1</a>
-                    </li>
-                    <li>
-                        <a href="#">Genre 2</a>
-                    </li>
-                    <li>
-                        <a href="#">Genre 3</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+       <?php
+
+    $page = 'genre2';
+
+    include('inc/menu.inc');
+
+    ?>
 
     <!-- Content -->
     <div class="container">
