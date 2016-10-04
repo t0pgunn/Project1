295d5d3ca23a0a3bd04ef4f6687091a7e6030af1
ddiazetc <ddiazetc@u.rochester.edu>
2016-10-03 21:51:46 -0400

    Added the colophon (says who did what)

---
+++ colophon.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Colophon</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/styles-genre.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+       <?php
+
+    $page = 'colophon';
+
+    include('inc/menu.inc');
+
+    ?>
+    <!-- Content -->
+    <div class="container">
+
+        <!-- Call to Action Well -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="well text-center">
+                    <h1>Who did what?</h1>
+                    <p>Gunnar Zemering</p>
+                    <p>Daniel Diaz-Etchevehere</p>
+                    <p>Riuari Conway</p>
+                    <p>Teron Russell</p>
+                    <p>Jake Socolow</p>
+
+                </div>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+
+        <!-- Footer -->
+    <?php
+
+    $page = 'home';
+
+    include('inc/footer.inc');
+
+    ?>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
